b3212dd76cb7c4613238b14ee32880ac12090bd0
Jo Liss <joliss42@gmail.com>
2013-06-18 16:29:11 -0700

    Remove reverse proxy middleware

--- configurations/connect.js
+++ configurations/connect.js
@@ -1,4 +1,3 @@
-var proxy = require('proxy-middleware');
 var url = require('url');
 var request = require('http').request;
 
@@ -8,39 +7,9 @@ module.exports = {
       port: 8000,
       hostname: '0.0.0.0',
       base: 'tmp/public',
-      middleware: middleware
     }
   }
 };
-
-function proxyIndex(req, res, next){
-  if (shouldProxyIndex(req.url)) {
-    // TODO: don't hardcode configuration
-
-    var theUrl = url.parse(req.url)
-    theUrl.headers = {};
-    theUrl.headers['Host'] = 'localhost:8000';
-    theUrl.hostname = '0.0.0.0';
-    theUrl.port = '3040';
-
-    var proxyReq =  request(theUrl, function (proxyRes) {
-      res.writeHead(proxyRes.statusCode, proxyRes.headers);
-      proxyRes.on('error', next);
-      proxyRes.pipe(res);
-    });
-
-    proxyReq.on('error', next);
-
-    if (!req.readable) {
-      proxyReq.end();
-    } else {
-      req.pipe(proxyReq);
-    }
-
-  } else {
-    next();
-  }
-}
 
 function blockDuringBuild(req,res,next){
   if (process.isLockedDuringBuild) {
@@ -57,27 +26,4 @@ function blockDuringBuild(req,res,next){
   } else {
     next();
   }
-}
-
-function middleware(connect, options) {
-  var theUrl;
-
-  theUrl = url.parse('http://localhost:3040/api');
-  theUrl.route = '/api';
-
-  return [
-    blockDuringBuild,
-    proxy(theUrl),
-    proxyIndex,
-    connect['static'](options.base),
-    connect.directory(options.base)
-  ];
-}
-
-var reservedRoutes = /^\/(vendor|css|js|cards|api|test)\//;
-var githubRepo = /^\/[^\/]+\/[^\/]+$/;
-
-function shouldProxyIndex(url) {
-  return url === "/" || url === "/index.html" ||
-    (!reservedRoutes.test(url) && githubRepo.test(url));
 }
